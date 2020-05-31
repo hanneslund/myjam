1425ba274f57cfc09f105b26ee65a762e57593d4
Hannes Lund <hannes.lund.2@consultant.volvo.com>
2020-05-31 10:32:17 +0200

    v0.4.1

---
+++ packages/myjam/runtime/context.ts
@@ -0,0 +1,20 @@
+// import { Context } from "../jsx-runtime";
+
+// type ContextObj<T> = {
+//   defaultValue: T;
+//   Provider: typeof Context;
+// };
+
+// export function createContext<T>(defaultValue: T) {
+//   const context: ContextObj<T> = {
+//     // self: context,
+//     defaultValue,
+//     Provider: Context,
+//   };
+
+//   return context;
+// }
+
+// // function useContext(context: ContextObj) {
+// //   return findParentContextNode(activeComp, context)
+// // }
